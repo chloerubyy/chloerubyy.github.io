7ca7e4dee053ea2490f24192946f6d3bf0f2649f
Chloe Rubinstein <chloe.rubinstein@wsu.edu>
2026-03-09 23:44:52 +0000

    made uodates

--- tic-tac-toe/scripts.js
+++ tic-tac-toe/scripts.js
@@ -1,8 +1,34 @@
 // declare the board data for a game, using 3 arrays
 // "-" indicates unmarked, "x" indicates an X mark, "o" indicates an O mark
-let rowA = [ "-", "-", "-" ];
-let rowB = [ "-", "-", "-" ];
-let rowC = [ "-", "-", "-" ];
+let rowA = ["-", "-", "-"];
+let rowB = ["-", "-", "-"];
+let rowC = ["-", "-", "-"];
+
+// track whos turn it is
+let currentTurn = "x";
+
+// set up a blank variable for current DOM element
+let currentPlayer;
+
+// function to handle clicks
+function clickSquare() {
+
+
+    // only proceed if space is empty
+    if (this.innerHTML == "") {
+        // set space
+        this.innerHTML = currentTurn;
+
+
+        // flip turn back and forth
+        if (currentTurn == "x") currentTurn = "o";
+        else currentTurn = "x";
+
+        // update next player DOM element
+        currentPlayer.innerHTML = currentTurn;
+    }
+
+}
 
 
 
@@ -15,6 +41,25 @@ function checkGameboard(a, b, c) {
 }
 
 
+
+// wait for the DOM to load before running the code
+document.addEventListener("DOMContentLoaded", function () {
+
+    // find all clickable spaces
+    let allSpaces = document.querySelectorAll(".gameSpace");
+
+    // loop with a "for-of" through all the clickable spaces
+    for (let eachSpace of allSpaces) {
+        eachSpace.addEventListener("click", clickSquare);
+    }
+
+    // update current DOM element with first player
+    let currentPlayer = document.querySelector("#currentPlayer span");
+    currentPlayer.innerHTML = currentTurn;
+});
+
+
+/* COMMENT OUT EVERYTHING BELOW THIS
 
 
 // get a handle on the DOM element to be updated with the outcome
@@ -38,4 +83,4 @@ if (winState == "x") {
   gameOutputMsg.innerHTML = "unknown";
 }
 
-
+*/
