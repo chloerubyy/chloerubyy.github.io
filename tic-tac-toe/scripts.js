85cff0f8e6cad94a6191fcf8ac4b2ecf83576037
Chloe Rubinstein <chloe.rubinstein@wsu.edu>
2026-03-10 03:52:16 +0000

    finished adding style changes to make it look prettier

--- tic-tac-toe/scripts.js
+++ tic-tac-toe/scripts.js
@@ -25,12 +25,14 @@ function clickSquare() {
 
     // set space
     this.innerHTML = currentTurn;
+    this.classList.add("clicked");
 
     // subtract 1 from remaining turns
     remainingTurns = remainingTurns - 1;
     console.log("Remaining turns: " + remainingTurns);
 
-    this.className = "gameSpace " + currentTurn;
+    // adds hover effect after click
+    this.classList.add(currentTurn);
 
     // update the array of boards with player value
     if (this.id == "a1") rowA[0] = currentTurn;
@@ -71,23 +73,47 @@ function clickSquare() {
       gameOver = true;
     }
 
+    // reveals game output if game is over
+    if (gameOver) {
+      document.querySelector("#gameResult").style.display = "block";
+    }
+
     // flip turn back and forth
-    if (currentTurn == "x") currentTurn = "o";
-    else currentTurn = "x";
+    currentTurn = (currentTurn === "x") ? "o" : "x";
 
 
-    // update next player DOM element
+    // update next player DOM element AND color
     currentPlayer.innerHTML = currentTurn;
+currentPlayer.style.color = (currentTurn === "x") ? "#F52798" : "#1E90FF";
   }
 }
 
 
-// return Boolean true if all 3 submitted values match, otherwise return Boolean false
-function spaceMatch(spaceA, spaceB, spaceC) {
+// returns true if all three spaces match and are not "-"
+function spaceMatch(a, b, c) { 
+  return a != "-" && a == b && b == c; 
 }
 
-// function to accept the 3 arrays and compare them
+// checks rows, columns, diagonals, and draw
 function checkGameboard(a, b, c) {
+  // check rows
+  if (spaceMatch(a[0], a[1], a[2])) return a[0];
+  if (spaceMatch(b[0], b[1], b[2])) return b[0];
+  if (spaceMatch(c[0], c[1], c[2])) return c[0];
+
+  // check columns
+  if (spaceMatch(a[0], b[0], c[0])) return a[0];
+  if (spaceMatch(a[1], b[1], c[1])) return a[1];
+  if (spaceMatch(a[2], b[2], c[2])) return a[2];
+
+  // check diagonals
+  if (spaceMatch(a[0], b[1], c[2])) return a[0];
+  if (spaceMatch(a[2], b[1], c[0])) return a[2];
+
+  // check draw
+  if (remainingTurns == 0) return "d";
+
+  return null;
 }
 
 
@@ -106,5 +132,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // update current DOM element with first player
   currentPlayer = document.querySelector("#currentPlayer span");
   currentPlayer.innerHTML = currentTurn;
+  currentPlayer.style.color = (currentTurn === "x") ? "#F52798" : "#1E90FF"; // added color to title to show whose turn it is
 });
+
+
 
