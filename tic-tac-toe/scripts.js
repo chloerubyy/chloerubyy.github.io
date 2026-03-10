20b9e79ef76207c12f3d4c83355bfd88b11614ac
Chloe Rubinstein <chloe.rubinstein@wsu.edu>
2026-03-10 03:15:01 +0000

    worked more struggling with some things not working tho

--- tic-tac-toe/scripts.js
+++ tic-tac-toe/scripts.js
@@ -7,6 +7,12 @@ let rowC = ["-", "-", "-"];
 // track whos turn it is
 let currentTurn = "x";
 
+// track number of turns left
+let remainingTurns = 9;
+
+// track if game is over
+let gameOver = false;
+
 // set up a blank variable for current DOM element
 let currentPlayer;
 
@@ -14,22 +20,66 @@ let currentPlayer;
 function clickSquare() {
 
 
-    // only proceed if space is empty
-    if (this.innerHTML == "") {
-        // set space
-        this.innerHTML = currentTurn;
+  // only proceed if space is empty
+  if ( (this.innerHTML == "") && (!gameOver) ) {
+
+    // set space
+    this.innerHTML = currentTurn;
+
+    // subtract 1 from remaining turns
+    remainingTurns = remainingTurns - 1;
+    console.log("Remaining turns: " + remainingTurns);
+
+    this.className = "gameSpace " + currentTurn;
+
+    // update the array of boards with player value
+    if (this.id == "a1") rowA[0] = currentTurn;
+    if (this.id == "a2") rowA[1] = currentTurn;
+    if (this.id == "a3") rowA[2] = currentTurn;
+    if (this.id == "b1") rowB[0] = currentTurn;
+    if (this.id == "b2") rowB[1] = currentTurn;
+    if (this.id == "b3") rowB[2] = currentTurn;
+    if (this.id == "c1") rowC[0] = currentTurn;
+    if (this.id == "c2") rowC[1] = currentTurn;
+    if (this.id == "c3") rowC[2] = currentTurn;
+
+    // output arrays to console
+    console.log("Rows:");
+    console.log(rowA);
+    console.log(rowB);
+    console.log(rowC);
 
 
-        // flip turn back and forth
-        if (currentTurn == "x") currentTurn = "o";
-        else currentTurn = "x";
+    // get a handle on the DOM element to be updated with the outcome
+    let gameOutputMsg = document.querySelector("#gameResult");
 
-        // update next player DOM element
-        currentPlayer.innerHTML = currentTurn;
+
+    // call your function checkGameboard() with the 3 rows
+    let winState = checkGameboard(rowA, rowB, rowC);
+
+    // test the returned value of the function
+    if (winState == "x") {
+      gameOutputMsg.innerHTML = "X wins";
+      gameOver = true;
+
+    } else if (winState == "o") {
+      gameOutputMsg.innerHTML = "O wins";
+      gameOver = true;
+
+    } else if ((winState == "d") && (remainingTurns == 0)) {
+      gameOutputMsg.innerHTML = "draw";
+      gameOver = true;
     }
 
-}
+    // flip turn back and forth
+    if (currentTurn == "x") currentTurn = "o";
+    else currentTurn = "x";
 
+
+    // update next player DOM element
+    currentPlayer.innerHTML = currentTurn;
+  }
+}
 
 
 // return Boolean true if all 3 submitted values match, otherwise return Boolean false
@@ -45,42 +95,16 @@ function checkGameboard(a, b, c) {
 // wait for the DOM to load before running the code
 document.addEventListener("DOMContentLoaded", function () {
 
-    // find all clickable spaces
-    let allSpaces = document.querySelectorAll(".gameSpace");
+  // find all clickable spaces
+  let allSpaces = document.querySelectorAll(".gameSpace");
 
-    // loop with a "for-of" through all the clickable spaces
-    for (let eachSpace of allSpaces) {
-        eachSpace.addEventListener("click", clickSquare);
-    }
+  // loop with a "for-of" through all the clickable spaces
+  for (let eachSpace of allSpaces) {
+    eachSpace.addEventListener("click", clickSquare);
+  }
 
-    // update current DOM element with first player
-    let currentPlayer = document.querySelector("#currentPlayer span");
-    currentPlayer.innerHTML = currentTurn;
+  // update current DOM element with first player
+  currentPlayer = document.querySelector("#currentPlayer span");
+  currentPlayer.innerHTML = currentTurn;
 });
 
-
-/* COMMENT OUT EVERYTHING BELOW THIS
-
-
-// get a handle on the DOM element to be updated with the outcome
-let gameOutputMsg = document.querySelector("#gameResult span");
-
-
-// call your function checkGameboard() with the 3 rows
-let winState = checkGameboard(rowA, rowB, rowC);
-
-// test the returned value of the function
-if (winState == "x") { 
-  gameOutputMsg.innerHTML = "X wins";
-  
-} else if (winState == "o") {
-  gameOutputMsg.innerHTML = "O wins";
-  
-} else if (winState == "d") {
-  gameOutputMsg.innerHTML = "draw";
-  
-} else {
-  gameOutputMsg.innerHTML = "unknown";
-}
-
-*/
