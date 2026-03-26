89c88403008b3ae3e2bd24eda22b4ab2010b1659
Chloe Rubinstein <chloe.rubinstein@wsu.edu>
2026-03-26 22:43:06 +0000

    completed video tutorial now going into make my own edits

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -2,74 +2,90 @@
 
 // track number of turns
 let playerClicks = 0;
+let timeDelay = 1000; // delay in milliseconds before cards flip back or disappear after a match
 
 // clear clicked classes
 function clearClicks() {
+
+    // get all clicked cards and remove the clicked class
     let allClickedCards = document.querySelectorAll(".clicked");
     for (let eachCard of allClickedCards) {
         eachCard.classList.remove("clicked");
-
-        // increase player turns by 1
-                playerClicks++;
-                document.querySelector("#turnCount span").innerHTML = playerClicks;
     }
 
-    // new function to flip cards when clicked
-    function flipCard() {
+    // increase player turns by 1
+    playerClicks++;
+    document.querySelector("#turnCount span").innerHTML = playerClicks;
 
-        if (!this.classList.contains("clicked") {
+    // check for winning
+    let allCards = document.querySelectorAll(".card");
+    let allMatchedCards = document.querySelectorAll(".matched");
+    if (allCards.length === allMatchedCards.length) {
+        // player has matched all cards
+        console.log("You win!");
+        document.querySelector("#winning").innerHTML = "You win!";
+    }
+}
 
-            // get all clicked cards
-            let allClickedCards = document.querySelectorAll(".clicked");
+// new function to flip cards when clicked
+function flipCard() {
 
-            // only proceed if there are less than 2 clicked cards
-            if (allClickedCards.length < 2) {
+    if (!this.classList.contains("clicked")) {
 
-                // add clicked class to the card that was clicked
-                this.classList.add("clicked");
+        // get all clicked cards
+        let allClickedCards = document.querySelectorAll(".clicked");
+
+        // only proceed if there are less than 2 clicked cards
+        if (allClickedCards.length < 2) {
+
+            // add clicked class to the card that was clicked
+            this.classList.add("clicked");
+        }
+
+        // get a fresh list of clicked cards
+        allClickedCards = document.querySelectorAll(".clicked");
+
+        // if its a pair, compare them
+        if (allClickedCards.length === 2) {
+
+            // get class list of each card as a string
+            let card1 = allClickedCards[0].classList.toString();
+            let card2 = allClickedCards[1].classList.toString();
+
+            // if the class lists are the same, its a match
+            if (card1 === card2) {
+                console.log("It's a match!");
+                allClickedCards[0].classList.add("matched");
+                allClickedCards[1].classList.add("matched");
+                window.setTimeout(clearClicks, timeDelay);
+
+            } else {
+                console.log("Not a match, try again.");
             }
 
-            // get a fresh list of clicked cards
-            allClickedCards = document.querySelectorAll(".clicked");
-
-            // if its a pair, compare them
-            if (allClickedCards.length === 2) {
-
-                // get class list of each card as a string
-                let card1 = allClickedCards[0].classList.toString;
-                let card2 = allClickedCards[1].classList.toString;
-
-                // if the class lists are the same, its a match
-                if (card1 === card2) {
-                    console.log("It's a match!");
-                    allClickedCards[0].classList.add("matched");
-                    allClickedCards[1].classList.add("matched");
-                    window.setTimeout(clearClicks, 2000);
-
-                } else {
-                    console.log("Not a match, try again.");
-                    clearClicks();
-                }
-            }
+            // whether matched or not, clear clicks
+            window.setTimeout(clearClicks, timeDelay);
 
         }
 
+    }
+}
 
+// run this code when the DOM content has loaded
+document.addEventListener('DOMContentLoaded', function (e) {
 
+    // get handles to game elements
+    let allCards = document.querySelectorAll(".card");
+    let gameboard = document.querySelector("#gameBoard");
 
-        // run this code when the DOM content has loaded
-        document.addEventListener('DOMContentLoaded', function (e) {
+    // loop through all cards and add click event listeners
+    for (let x = 0; x < allCards.length; x++) {
+        // randomize cards by reinserting them in random order in the gameboard
+        let randNum = Math.floor(Math.random() * allCards.length);
+        gameboard.insertBefore(allCards[x], gameboard.children[randNum]);
 
-            // get handles to game elements
-            let allCards = document.querySelectorAll(".card");
-            let gameboard = document.querySelector("#gameBoard");
+        // call flipCard() when a card is clicked
+        allCards[x].addEventListener("click", flipCard);
+    }
 
-            // randomizing cards by looping through all cards
-            for (x = 0; x < allCards.length; x++) {
-                let randNum = Math.floor(Math.random() * allCards.length);
-                gameboard.insertBefore(allCards[x], gameboard.children[randNum]);
-
-                allCards[x].addEventListener("click", flipCard);
-            }
-
-        });
+});
