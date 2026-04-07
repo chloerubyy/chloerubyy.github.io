2d031ff31d7cca321bf0c673a9c54f2ea7fd63c9
Chloe Rubinstein <chloe.rubinstein@wsu.edu>
2026-04-07 23:51:36 +0000

    made more progress

--- weather-app/scripts.js
+++ weather-app/scripts.js
@@ -1,10 +1,34 @@
-/* javascript to enable drag-scrolling for weather app */
+/* javascript to enable drag-scrolling for weather app 2.0 */
 
+// preparing variables 
 let scrollingBox;
 let offsetLeftStart;
 let scrollLeftStart;
 let isMoving;
 
+
+// function to get remote JSON data 
+async function getData(url, options) {
+    try {
+        const response = await fetch(url, options);
+        if (response.ok) {
+            const result = await response.json();
+            return result;
+        } else {
+            throw (response.status);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// update weather display in the DOM based on passed object
+function updateWeather(weatherObject) {
+   console.log(weatherObject);
+}
+
+
+// wait for DOM to load
 document.addEventListener("DOMContentLoaded", function () {
     scrollingBox = document.querySelector("#futureInfo");
     isMoving = false;
@@ -58,4 +82,15 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!isMoving) return;
         scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
     });
+
+    let sampleURL = "https://tordevries.github.io/477/examples/ajax-api-test/current-forecast.js" ;
+    let sampleOptions = {};
+
+    // get sample data
+    getData(sampleURL, sampleOptions).then(function (result) {
+        //code to operate on "result" JSON obkect
+        updateWeather(result);
+    });
+
+
 });
