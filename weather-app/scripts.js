6505dcafc72171d40db6a497802030075fa3f559
Chloe Rubinstein <chloe.rubinstein@wsu.edu>
2026-04-07 23:17:40 +0000

    created files for TP12 and added to homepage

---
+++ weather-app/scripts.js
@@ -0,0 +1,61 @@
+/* javascript to enable drag-scrolling for weather app */
+
+let scrollingBox;
+let offsetLeftStart;
+let scrollLeftStart;
+let isMoving;
+
+document.addEventListener("DOMContentLoaded", function () {
+    scrollingBox = document.querySelector("#futureInfo");
+    isMoving = false;
+
+    scrollingBox.addEventListener("mousedown", function (e) {
+        scrollLeftStart = scrollingBox.scrollLeft;
+        offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
+        isMoving = true;
+    });
+
+    scrollingBox.addEventListener("mouseleave", function () {
+        isMoving = false;
+    });
+
+    scrollingBox.addEventListener("mouseup", function () {
+        isMoving = false;
+    });
+
+    scrollingBox.addEventListener("mousemove", function (e) {
+        e.preventDefault();
+        if (!isMoving) return;
+        scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
+    });
+});/* javascript to enable drag-scrolling for weather app */
+
+let scrollingBox;
+let offsetLeftStart;
+let scrollLeftStart;
+let isMoving;
+
+document.addEventListener("DOMContentLoaded", function () {
+    scrollingBox = document.querySelector("#futureInfo");
+    isMoving = false;
+
+    scrollingBox.addEventListener("mousedown", function (e) {
+        scrollLeftStart = scrollingBox.scrollLeft;
+        offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
+        isMoving = true;
+    });
+
+    scrollingBox.addEventListener("mouseleave", function () {
+        isMoving = false;
+    });
+
+    scrollingBox.addEventListener("mouseup", function () {
+        isMoving = false;
+    });
+
+    scrollingBox.addEventListener("mousemove", function (e) {
+        e.preventDefault();
+        if (!isMoving) return;
+        scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
+    });
+});
