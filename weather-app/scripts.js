fd3dcc5627fbdaf049f8306b54b4ed787cfb4c05
Chloe Rubinstein <chloe.rubinstein@wsu.edu>
2026-04-08 01:08:19 +0000

    fixed some typos messing up data

--- weather-app/scripts.js
+++ weather-app/scripts.js
@@ -24,7 +24,35 @@ async function getData(url, options) {
 
 // update weather display in the DOM based on passed object
 function updateWeather(weatherObject) {
-   console.log(weatherObject);
+
+    // outputting whole weather object to console
+    console.log(weatherObject);
+
+    // update current weather info
+    document.querySelector("#currentTemp span").innerHTML = weatherObject.current.temp_f;
+    document.querySelector("#currentStatus").innerHTML =
+        `<img src="${weatherObject.current.condition.icon}" class="weatherIcon"> ${weatherObject.current.condition.text}`;
+    document.querySelector("#currentHumidity span").innerHTML = weatherObject.current.humidity;
+
+    // output wind speed and direction in a combined string
+    let windspeed = weatherObject.current.wind_mph;
+    let winddirection = weatherObject.current.wind_dir;
+    document.querySelector("#currentWind").innerHTML = windspeed + "mph " + winddirection;
+
+    // find all future day blocks and loop through them, matching the forecast days in the weather object
+    let futureDays = document.querySelectorAll(".futureDay");
+    for (let i = 0; i < futureDays.length; i++) {
+
+        // update future temp
+        futureDays[i].querySelector(".futureTemp span").innerHTML = weatherObject.forecast.forecastday[i].day.maxtemp_f;
+
+        // update future windspeed
+        windspeed = weatherObject.forecast.forecastday[i].day.maxwind_mph;
+        futureDays[i].querySelector(".futureWind").innerHTML = windspeed + "mph";
+
+        // update future condition status
+        futureDays[i].querySelector(".futureStatus").innerHTML = weatherObject.forecast.forecastday[i].day.condition.text;
+    }
 }
 
 
@@ -52,38 +80,8 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!isMoving) return;
         scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
     });
-});/* javascript to enable drag-scrolling for weather app */
 
-let scrollingBox;
-let offsetLeftStart;
-let scrollLeftStart;
-let isMoving;
-
-document.addEventListener("DOMContentLoaded", function () {
-    scrollingBox = document.querySelector("#futureInfo");
-    isMoving = false;
-
-    scrollingBox.addEventListener("mousedown", function (e) {
-        scrollLeftStart = scrollingBox.scrollLeft;
-        offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
-        isMoving = true;
-    });
-
-    scrollingBox.addEventListener("mouseleave", function () {
-        isMoving = false;
-    });
-
-    scrollingBox.addEventListener("mouseup", function () {
-        isMoving = false;
-    });
-
-    scrollingBox.addEventListener("mousemove", function (e) {
-        e.preventDefault();
-        if (!isMoving) return;
-        scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
-    });
-
-    let sampleURL = "https://tordevries.github.io/477/examples/ajax-api-test/current-forecast.js" ;
+    let sampleURL = "https://tordevries.github.io/477/examples/ajax-api-test/current-forecast.js";
     let sampleOptions = {};
 
     // get sample data
